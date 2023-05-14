b01d587d25ba6aa540206c3230946876cae6d06f
TheForeverOptimist <sean@elevate.app>
2023-05-14 13:49:54 -0400

    added archictecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './components/header/header'
 
-function App() {
+const App = () => {
   return (
-    <div>App</div>
+   <>
+    <Header />
+   </>
   )
 }
 
